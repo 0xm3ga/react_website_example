b132739b7b3799443c3864a33a14eac96df8bd9e
front-end-guy-2020 <56974140+0xm3ga@users.noreply.github.com>
2023-07-17 19:09:52 +0100

    added ci

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -6,4 +6,12 @@ module.exports = {
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   collectCoverage: true,
+  moduleNameMapper: {
+    "^.+\\.(css|less|scss)$": "identity-obj-proxy",
+    "^.+\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/fileMock.js",
+  },
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["./src/setupTests.ts"],
+  watchman: false,
 };
